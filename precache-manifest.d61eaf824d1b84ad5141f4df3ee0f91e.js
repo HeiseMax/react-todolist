b266e5e8dcd5dd262f586ad582596c989de3821f
Max Heise <max.heise@gmx.de>
2020-10-01 15:28:05 +0200

    Updates

---
+++ precache-manifest.d61eaf824d1b84ad5141f4df3ee0f91e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba363f08b718dd599fcc61cb9fe3af2b",
+    "url": "/github.io/react-todolist/index.html"
+  },
+  {
+    "revision": "c5fe52f8ad4b6c5314d2",
+    "url": "/github.io/react-todolist/static/js/2.287bfea5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/react-todolist/static/js/2.287bfea5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c601fa68e5c99d80be0b",
+    "url": "/github.io/react-todolist/static/js/main.f098f4bc.chunk.js"
+  },
+  {
+    "revision": "ff131f7f1ba57247649e",
+    "url": "/github.io/react-todolist/static/js/runtime-main.61c72cb6.js"
+  }
+]);
